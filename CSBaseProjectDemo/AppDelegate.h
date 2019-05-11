a05886985ce6653046139810e7b9da6b2be7a04a
陈双 <chenshuang@banggood.com>
2019-05-11 14:28:49 +0800

    Initial Commit

---
+++ CSBaseProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CSBaseProjectDemo
+//
+//  Created by cs on 2019/5/11.
+//  Copyright © 2019 cs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CSBaseProjectDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CSBaseProjectDemo
+//
+//  Created by cs on 2019/5/11.
+//  Copyright © 2019 cs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
